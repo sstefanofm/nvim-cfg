9e35311ad2d37d0b4599f7d59b9d22341350ce3b
stf <sstefanofm@gmail.com>
2025-06-21 18:04:58 -0300

    paste without loosing buffer

--- lua/stf/remap.lua
+++ lua/stf/remap.lua
@@ -4,3 +4,6 @@ vim.keymap.set('n', ';', ':')
 
 vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
 vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+
+-- paste without loosing the yanked buffer
+vim.keymap.set('x', '<leader>p', '"_dP')
