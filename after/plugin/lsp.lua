bb036737cdbc1a64a86c0963044f3c9dc6659c3d
stf <sstefanofm@gmail.com>
2025-06-21 19:31:25 -0300

    some configs for lspconfig. added lua_ls

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,3 +1,10 @@
 local lsp = require 'lspconfig'
 
-lsp.tsserver.setup {}
+vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, {})
+vim.keymap.set('n', 'K',  vim.lsp.buf.hover, {})
+vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, {})
+
+lsp.lua_ls.setup {}
+
+-- lsp.tsserver.setup {}
--- lua/stf/remap.lua
+++ lua/stf/remap.lua
@@ -11,8 +11,7 @@ map('v', 'K', ":m '<-2<CR>gv=gv")
 map('x', '<leader>p', '"_dP')
 
 -- copy to system clipboard
-map('n', '<leader>y', '"+y')
-map('v', '<leader>y', '"+y')
+map({ 'n', 'v' }, '<leader>y', '"+y')
 map('n', '<leader>Y', '"+Y')
 
 -- replace word under cursor
