13ebc034c85db711bb51d1b9e33267fac68b0ec4
stf <sstefanofm@gmail.com>
2025-06-21 18:37:59 -0300

    enable indent treesitter

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,7 +1,18 @@
 local builtin = require('telescope.builtin')
 
-vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>fg', builtin.git_files,  { desc = 'Telescope find git files only' })
-vim.keymap.set('n', '<leader>fs', function()
-  builtin.grep_string({ search = vim.fn.input('Grep > ') })
-end, { desc = 'Telescope find files with searched string' })
+vim.keymap.set('n', '<leader>ff', builtin.find_files,
+  { desc = 'Telescope find files' }
+)
+
+vim.keymap.set('n', '<leader>fg', builtin.git_files,
+  { desc = 'Telescope find git files only' }
+)
+
+-- vim.keymap.set('n', '<leader>fs', function() -- does not work?? ???
+--   builtin.grep_string({ search = vim.fn.input('Grep > ') })
+-- end, { desc = 'Telescope find files with searched string' })
+
+-- why does not wokrr!!!!?
+-- vim.keymap.set('n', '<leader>fs', builtin.live_grep,
+--   { desc = 'Telescope find files with searched string' }
+-- )
