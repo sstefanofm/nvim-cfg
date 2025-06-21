0fceb44f0c2ffa5a5ddc82aebfab00df8934c639
stf <sstefanofm@gmail.com>
2025-06-21 18:07:27 -0300

    yank to system clipboard

--- lua/stf/remap.lua
+++ lua/stf/remap.lua
@@ -7,3 +7,8 @@ vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
 
 -- paste without loosing the yanked buffer
 vim.keymap.set('x', '<leader>p', '"_dP')
+
+-- copy to system clipboard
+vim.keymap.set('n', '<leader>y', '"+y')
+vim.keymap.set('v', '<leader>y', '"+y')
+vim.keymap.set('n', '<leader>Y', '"+Y')
