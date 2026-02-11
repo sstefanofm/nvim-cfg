0113df2cb938d4e3c34073276bfcb0dcd71fd6ed
stf <stf@arch.lan>
2026-02-11 18:51:55 -0300

    enable lsp completions

--- after/plugin/completions.lua
+++ after/plugin/completions.lua
@@ -53,7 +53,7 @@ cmp.setup({
 		["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
 	}),
 	sources = cmp.config.sources({
-		-- { name = "nvim_lsp" },
+		{ name = "nvim_lsp" },
 		-- { name = "vsnip" }, -- For vsnip users.
 		{ name = "luasnip" }, -- For luasnip users.
 		-- { name = 'ultisnips' }, -- For ultisnips users.
